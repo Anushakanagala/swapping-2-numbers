82b62e86ad49dec25afe592d7944c665fc65c94b
Anushakanagala <67909209+Anushakanagala@users.noreply.github.com>
2020-07-06 19:16:42 +0530

    created swap.c
    shows output numbers after swap

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+printf("enter number a:");
+scanf("%d",&a);
+printf("enter number b:");
+scanf("%d",&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("after swap a:%d,b:%d\n,a,b);
